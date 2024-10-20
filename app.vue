230a89f48d00140d0584c11f62bc8975688a89bd
muhamadrofii <muhamadrofii911@gmail.com>
2024-10-20 21:21:02 +0700

    add useSeoMeta

--- app.vue
+++ app.vue
@@ -3,11 +3,18 @@ useSeoMeta({
   title: 'Muhamad Rofi\'i - Personal Portfolio',
   ogTitle: 'Muhamad Rofi\'i - Personal Portfolio',
   description: 'Welcome to the personal portfolio of Muhamad Rofi\'i. Explore my projects, skills, and experiences.',
-  ogDescription: 'Discover the work and expertise of Muhamad Rofi\'i, a skilled professional backend developer',
-  // ogImage: 'https://example.com/image.png',
+  ogDescription: 'Discover the work and expertise of Muhamad Rofi\'i, a skilled professional in the tech industry.',
+  // ogImage: 'https://example.com/image.png', 
   // twitterCard: 'summary_large_image',
-})
+});
+
+useHead({
+  meta: [
+    { name: 'keywords', content: 'BEK EN EMJINER, portfolio, Muhamad Rofi\'i, Nuxt.js, BEK EN EMJINER, JavaScript' }
+  ]
+});
 </script>
+
 
 <template>
   <NuxtLayout>
